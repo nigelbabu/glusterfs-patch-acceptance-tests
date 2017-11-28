f51a7f198ae2676c211cb4ad77c8b2f4b0535de5
Nigel Babu <nigelb@redhat.com>
2017-11-28 14:01:10 +0530

    Add more clean up steps
    See: http://git.gluster.org/cgit/build-jobs.git/tree/build-gluster-org/scripts/centos-regression.sh#n34

--- cleanup.sh
+++ cleanup.sh
@@ -15,12 +15,17 @@ function cleanup()
     killall -9 glusterfs glusterfsd glusterd glusterd 2>&1 || true;
     pkill /opt/qa/regression.sh 2>&1
     umount -l $M 2>&1 || true;
-    rm -rf /var/lib/glusterd/* /etc/glusterd/* /var/log/glusterfs/* $P/export;
+    rm -rf /build/install /build/scratch /var/lib/glusterd/* /etc/glusterd/* /var/log/glusterfs/* $P/export;
+    sudo rm -f /var/run/????????????????????????????????.socket >/dev/null 2>&1
 }
 
 
 function main ()
 {
+    JDIRS="/var/log/glusterfs /var/lib/glusterd /var/run/gluster /d /d/archived_builds /d/backends /d/build $WORKSPACE"
+    sudo mkdir -p $JDIRS
+    sudo chown jenkins:jenkins $JDIRS
+    chmod 755 $JDIRS
     cleanup;
 }
 
